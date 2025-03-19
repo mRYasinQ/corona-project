0c17931fdc409056f13b4eb5096b70a977b5a91f
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-03-19 04:38:13 +0330

    feat: add chart section

---
+++ assets/scripts/app.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const chart1 = document.getElementById('chart-1');
+    const chart2 = document.getElementById('chart-2');
+
+    const labels = ['Jan', 'Feb', 'March', 'April', 'May', 'June', 'July', 'Aug'];
+    const config = {
+        type: 'line',
+        data: {
+            datasets: null,
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+            scales: {
+                y: {
+                    min: 0,
+                    max: 100,
+                    beginAtZero: true,
+                },
+            },
+        },
+    };
+
+    const chart1Data = {
+        labels,
+        datasets: [
+            {
+                data: [10, 25, 17, 34, 42, 27, 35, 41],
+                borderColor: '#11e5b4',
+                borderWidth: 2,
+            },
+        ],
+    };
+    const chart2Data = {
+        labels,
+        datasets: [
+            {
+                data: [60, 40, 49, 42, 52, 39, 28, 5],
+                borderColor: '#F85252',
+                borderWidth: 2,
+            },
+        ],
+    };
+
+    const chartAsc = new Chart(chart1, { ...config, data: chart1Data });
+    const chartDesc = new Chart(chart2, { ...config, data: chart2Data });
+});
