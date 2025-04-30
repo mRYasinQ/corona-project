5c4f046d81e5516fe2e367f714d4605f89cf0774
Yasin Abbasi <yasinabbasi.y20@gmail.com>
2025-04-30 17:58:46 +0330

    feat: connect chart, today and worldwide statistics to API

--- assets/scripts/modules/statistics.js
+++ assets/scripts/modules/statistics.js
@@ -3,47 +3,81 @@ import handleError from '../exception/handleError.js';
 import DefaultCountry from '../constants/defaultCountry.js';
 import displayStatistics from '../constants/displayStatistics.js';
 import { getCountryStats } from '../services/statisticsService.js';
+import { formatDate, getLastDayOfMonth } from '../utility/date.js';
 
+const STATS_YEAR = 2021;
 const filterForm = document.querySelector('.statistic-form');
 const dateInput = filterForm.querySelector('input[type="date"]');
 const countryStatsBox = document.querySelector('.statistic-content-bottom');
-let filterCStatsController = null;
+const todayStatsBox = document.querySelector('#today-statistic > .data-table-content');
+const wordlStatsBox = document.querySelector('#world-statistic > .data-table-content');
+const recoveredChart = document.querySelector('#chart-recovered');
+const deathChart = document.querySelector('#chart-death');
+let filterController = null;
+let todayController = null;
 
 async function initStatistics() {
+    await showWorldStats();
+    await showChartStats();
+
     filterForm?.addEventListener('submit', filterCountryStats);
 }
 
 async function filterCountryStats(event) {
     event.preventDefault();
 
-    if (filterCStatsController) {
-        filterCStatsController.abort();
-    }
+    if (filterController) filterController.abort();
+    if (todayController) todayController.abort();
 
     const formData = new FormData(filterForm);
     const iso = formData.get('country');
     const date = formData.get('date') || null;
+    const today = formatDate(new Date());
 
     try {
         if (iso == DefaultCountry.value) throw new AppError('Select a country!');
 
-        filterCStatsController = new AbortController();
-        const countryStats = await getCountryStats(iso, date, filterCStatsController.signal);
+        filterController = new AbortController();
+        todayController = new AbortController();
 
-        showCountryStats(countryStats);
+        const [countryStats, todayCountryStats] = await Promise.all([
+            getCountryStats(iso, date, filterController.signal),
+            getCountryStats(iso, today, todayController.signal),
+        ]);
+
+        showFilterStats(countryStats);
+        showBoxStats(todayCountryStats, todayStatsBox);
     } catch (error) {
         handleError(error);
     }
 }
 
-function showCountryStats(stats) {
+async function showWorldStats() {
+    const wordlStats = await getCountryStats('0', getLastDayOfMonth(STATS_YEAR, 12));
+
+    showBoxStats(wordlStats, wordlStatsBox);
+}
+
+async function showChartStats() {
+    const month = [1, 2, 3, 4, 5, 6];
+
+    const worldStats = await Promise.all(month.map((m) => getCountryStats('0', getLastDayOfMonth(STATS_YEAR, m))));
+
+    const recovered = worldStats.map((stat) => stat?.data?.recovered ?? 0);
+    const death = worldStats.map((stat) => stat?.data?.deaths ?? 0);
+
+    createChart(recoveredChart, recovered, '#11e5b4');
+    createChart(deathChart, death, '#F85252');
+}
+
+function showFilterStats(stats) {
     countryStatsBox.innerHTML = '';
 
     displayStatistics.forEach(({ label, keyData }) => {
-        const value = stats.data[keyData] ?? 'N/N';
+        const value = stats.data[keyData] ?? 0;
         let valueClass = 'statistic-content-value';
 
-        if (label === 'Death' && typeof value === 'number') {
+        if (label === 'Death') {
             if (value >= 500) valueClass += ' text-yellow';
         }
 
@@ -56,6 +90,59 @@ function showCountryStats(stats) {
         </div>`;
         countryStatsBox.insertAdjacentHTML('beforeend', statsElement);
     });
+}
+
+function showBoxStats(stats, dataBox) {
+    dataBox.innerHTML = '';
+
+    displayStatistics.forEach(({ label, keyData }) => {
+        const value = stats.data[keyData] ?? 0;
+        let valueClass = 'data-table-value';
+
+        if (label === 'Death') {
+            if (value >= 500) valueClass += ' text-yellow';
+        }
+
+        if (label === 'Recovered') {
+            if (value >= 500) valueClass += ' text-primary';
+        }
+
+        const statsElement = `
+        <div class="data-table-item">
+            <span class="${valueClass}">${value.toLocaleString()}</span>
+            <span class="data-table-label">${label}</span>
+        </div>`;
+        dataBox.insertAdjacentHTML('beforeend', statsElement);
+    });
+}
+
+function createChart(chartElement, data, borderColor) {
+    const labels = ['Jan', 'Feb', 'March', 'April', 'May', 'June'];
+    const config = {
+        type: 'line',
+        data: {
+            labels,
+            datasets: [
+                {
+                    data,
+                    borderColor,
+                    borderWidth: 2,
+                },
+            ],
+        },
+        options: {
+            responsive: false,
+            plugins: {
+                legend: {
+                    display: false,
+                },
+            },
+        },
+    };
+
+    const chart = new Chart(chartElement, config);
+
+    return chart;
 }
 
 export { initStatistics };
---
+++ assets/scripts/utility/date.js
@@ -0,0 +1,17 @@
+function formatDate(date) {
+    const d = new Date(date);
+
+    let year = d.getFullYear();
+    let month = String(d.getMonth() + 1).padStart(2, 0);
+    let day = String(d.getDate()).padStart(2, 0);
+
+    const result = `${year}-${month}-${day}`;
+
+    return result;
+}
+
+function getLastDayOfMonth(year, month) {
+    return formatDate(new Date(year, month, 0));
+}
+
+export { formatDate, getLastDayOfMonth };
